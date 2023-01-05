1f4d0e3ee8ddb3a22d4300eecafc0bc69afddd28
Kishore B <kishor.b.mca@gmail.com>
2023-01-05 11:17:33 +0530

    Automation script for mongo db server

---
+++ mongodb.sh
@@ -0,0 +1,16 @@
+HOME_DIR=$(pwd)
+
+cp ${HOME_DIR}/files/mongo.repo /etc/yum.repos.d/mongo.repo
+
+#install mongo db
+yum install mongodb-org -y
+
+#Enable and start service
+systemctl enable mongod
+systemctl start mongod
+
+#replace default bind ID with 0.0.0.0
+sed -i -e 's/127.0.0.0/0.0.0.0/'  /etc/mongod.conf
+
+#restart the mongo db service
+systemctl restart mongod
