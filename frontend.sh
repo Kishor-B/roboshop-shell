164123516e89c2183ed581350569a22a772b7d15
Kishore B <kishor.b.mca@gmail.com>
2023-01-05 10:54:57 +0530

    shell to demonistrate print

---
+++ frontend.sh
@@ -0,0 +1,21 @@
+HOME_DIR=$(pwd)
+yum install nginx -y
+
+sytemctl enable nginx
+
+systemctl start nginx
+
+#Change the default index.html that nginx webserver is serving
+rm -rf /usr/share/nginx/html/*
+
+#Down load content for front-end.
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+#Extract the front end content.
+cd /usr/share/nginx/html/
+unzip /tmp/frontend.zip
+
+#Create nginx reverse proxy file
+cp  ${HOME_DIR}/files/nginx-roboshop-rev-proxy.conf  /usr/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
